2f12001e481ab6546f1e825f9530cf78c29002ca
carverh <pcarverb@hotmail.com>
2017-04-17 12:48:40 -0700

    Added stuff

---
+++ core/init.php
@@ -0,0 +1,11 @@
+<?php
+	/*
+	 * DorpCMS
+	 * Copyright 2017 Carver Harrison
+	 * init.php
+	*/
+	
+	requre_once("loaders/models.php");
+	requre_once("loaders/content_types.php");
+	requre_once("loaders/views.php");
+?>
