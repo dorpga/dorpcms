e332c713b12ca7dd8b7b39b63c082c29e310141b
carverh <pcarverb@hotmail.com>
2017-04-18 12:34:02 -0700

    edits

--- core/render.php
+++ core/render.php
@@ -3,14 +3,30 @@
 class Renderer {
   function renderPageFromDom ($contentXml) {
     // Load content type
-    $contentType = ContentTypeLoader::load($contentXml->getElementsByTagName('Type')[0]->nodeValue);
+    $contentTypeResponse = ContentTypeLoader::load($contentXml->getElementsByTagName('Type')[0]->nodeValue);
 
     // Check for errors
-    if (isset($contentType['error'])) {
-      // Echp error
-      echo 'Renderer: ContentTypeLoader: Error '.$contentType['error'];
+    if (isset($contentTypeResponse['error'])) {
+
+      // Echo error
+      echo 'Renderer/ContentTypeLoader/Error '.$contentTypeResponse['error'];
+
     } else {
-      echo 'OK';
+
+      // Get xml
+      $contentType = $contentTypeResponse['object'];
+
+      // Get model node
+      $contentModel = $contentType->getElementsByTagName('Model')[0];
+
+      // Check if src attribute exists
+      if ($contentModel->getAttribute('src')) {
+        // Set model source variable
+        $contentModelSrc = $contentModel->getAttribute('src');
+
+        // Load model xml
+        $contentModelXml = DOMDocument::load(__DIR__.'/models/'.$contentModelSrc.'.xml');
+      }
     }
   }
 }
